57fc6b676dcd8f2663f15ef75d7e79aece36e5c4
Antonio Signore <antonio@threebluecircles.com>
2016-09-06 08:40:54 +0200

    removed few System.out.println

--- src/main/java/com/jpmorgan/utils/StockPredicate.java
+++ src/main/java/com/jpmorgan/utils/StockPredicate.java
@@ -22,18 +22,11 @@ public class StockPredicate implements Predicate {
     public boolean evaluate(Object tradeObject) {
         Trade trade = (Trade) tradeObject;
 
-        System.out.println("instrument.getName() = " + instrument.getName());
-        System.out.println("trade.getInstrument().getName() = " + trade.getInstrument().getName());
-        System.out.println("dateRange.getTime() = " + dateRange.getTime());
-        System.out.println("trade.getTradeDate() = " + trade.getTradeDate());
-
         boolean toInclude = trade.getInstrument().equals(instrument);
         if (toInclude && dateRange != null) {
             toInclude = dateRange.getTime().compareTo(trade.getTradeDate()) <= 0;
-            System.out.println("dateRange.getTime().compareTo(trade.getTradeDate())<=0 = " + (dateRange.getTime().compareTo(trade.getTradeDate()) <= 0));
         }
 
-        System.out.println("toInclude = " + toInclude);
         return toInclude;
     }
 }
